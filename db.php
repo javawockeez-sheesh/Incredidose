7261917c18025b1a33a974e0dcbf6e0fdd92b2e8
SeguirreJoshua <andreijoshua.seguirre@gmail.com>
2025-12-12 19:47:43 +0800

    wordington commit

--- db.php
+++ db.php
@@ -1,9 +1,13 @@
 <?php
-    $dbserver = "db";
+    $dbserver = "localhost";
     $dbuser = "root";
-    $dbpass = "123";
+    $dbpass = "";
     $dbname = "incredidose";
     $port = 3306;
 
     $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname, $port);
+
+     if ($db->connect_error) {
+        die("Database connection failed: " . $db->connect_error);
+    }
 ?>
