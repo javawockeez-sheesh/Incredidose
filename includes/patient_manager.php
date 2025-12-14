bd5040a2eec6ff5d9e69bbd3609138cdc654f4e5
John Lex Paulo Palaris <pfantom39@gmail.com>
2025-12-15 00:05:50 +0800

    fuck

--- includes/patient_manager.php
+++ includes/patient_manager.php
@@ -29,6 +29,19 @@ function getPatientByName($doctorid, $patientname) {
 	return $data;
 }
 
+function getPatientByEmail($email) {
+	global $db;
+    $stmt = $db->prepare("SELECT u.userid, u.firstname, u.lastname, u.email, u.contactnum, MAX(p.dateprescribed) AS dateprescribed FROM user u JOIN prescription p ON u.userid = p.patientid WHERE u.email = ?");
+    $stmt->execute([$emails]);
+    $result = $stmt->get_result();
+    $data = [];
+    while ($row = $result->fetch_assoc()) {
+    	$data[] = $row;
+	}
+	return $data;
+}
+
+
 function getPatientById($patientid) {
 	global $db;
 	$stmt = $db->prepare("SELECT * from User where userid = ?");
@@ -57,6 +70,11 @@ switch ($action) {
 		echo json_encode(getPatientByName($doctorid, $patientname));
 		break;
 
+	case "getPatientByEmail":
+		$email = $_GET['email'];
+		header('Content-Type: application/json');
+		echo json_encode(getPatients($email));
+		break;
 
 	case "getPatientById":
 		$patientid = $_GET['patientid'];
